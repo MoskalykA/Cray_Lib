ecd5b5ef6fcb87bf95d98e6a6a92d577572fe71a
MoskalykA <45717749+MoskalykA@users.noreply.github.com>
2020-06-21 10:57:40 +0200

    Update

---
+++ lua/vgui/cray_dbutton.lua
@@ -0,0 +1,14 @@
+local PANEL = {}
+function PANEL:Init()
+    self:SetTextColor(color_white)
+end
+
+function PANEL:Paint( w, h )	
+    if self:IsHovered() then
+        draw.RoundedBox(5, 0, 0, w, h, Color(52, 152, 219))
+    else
+        draw.RoundedBox(5, 0, 0, w, h, Color(41, 128, 185))
+    end	
+end
+
+vgui.Register("Cray::DButton", PANEL, "DButton")
---
+++ lua/vgui/cray_dframe.lua
@@ -0,0 +1,11 @@
+local PANEL = {}
+function PANEL:Init()
+    self:SetTitle('')
+    self:MakePopup()
+end
+
+function PANEL:Paint( w, h )	
+    draw.RoundedBox(10, 0, 0, w, h, Color(26, 32, 44))	
+end
+
+vgui.Register("Cray::DFrame", PANEL, "DFrame")
